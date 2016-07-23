bc2ca50aa541bbe780cc16b6595936c465e18ac4
Brad Gessler <bradgessler@gmail.com>
2016-07-23 13:14:46 -0700

    Fix CodeClimate test reporter

--- mascot-server/spec/spec_helper.rb
+++ mascot-server/spec/spec_helper.rb
@@ -1,4 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require "codeclimate-test-reporter"
+CodeClimate::TestReporter.configure do |config|
+  config.git_dir = `git rev-parse --show-toplevel`.strip
+end
 CodeClimate::TestReporter.start
