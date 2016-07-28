1c0690beff628459933d442a2e45333a3d9417ce
Brad Gessler <bradgessler@gmail.com>
2016-07-27 23:06:05 -0700

    Resources observes Resource#request_path changes to improve performance

--- mascot/lib/mascot/resource.rb
+++ mascot/lib/mascot/resource.rb
@@ -1,19 +1,44 @@
 require "forwardable"
+require "observer"
 
 module Mascot
   # Represents the request path of an asset. There may be multiple
   # resources that point to the same asset. Resources are immutable
   # and may be altered by the resource proxy.
   class Resource
-    attr_accessor :request_path, :asset
-    attr_writer :body, :data
+    include Observable
 
     extend Forwardable
     def_delegators :asset, :mime_type
 
+    attr_accessor :request_path, :asset
+    attr_writer :body, :data
+
     def initialize(request_path: , asset: )
-      @request_path = request_path
+      self.request_path = request_path
       @asset = asset
+    end
+
+    # When #dup or #clone is copied, the Resource
+    # collection observer must be removed so there's
+    # not duplicate resources.
+    def initialize_copy(instance)
+      instance.delete_observers
+      super instance
+    end
+
+    def request_path=(request_path)
+      old_request_path = @request_path
+      # We freeze the value to ensure users can't modify
+      # the request_path string in place (e.g. Resource#request_path.capitalize!)
+      # and throw the resource out of sync with the Resources collection.
+      @request_path = request_path.dup.freeze
+      changed
+      notify_observers self, old_request_path
+    end
+
+    def inspect
+      "#<#{self.class}:0x#{(object_id << 1).to_s(16)} @request_path=#{@request_path.inspect} @asset=#{@asset.inspect}>"
     end
 
     def data
--- mascot/lib/mascot/resources.rb
+++ mascot/lib/mascot/resources.rb
@@ -3,15 +3,19 @@ module Mascot
     include Enumerable
 
     extend Forwardable
-    def_delegators :@resources, :size, :empty?, :any?, :clear, :last
+    def_delegators :@routes, :size, :empty?, :any?, :clear
 
     def initialize(root_file_path: )
-      @resources = Array.new
+      @routes = Hash.new
       @root_file_path = Pathname.new(root_file_path)
     end
 
     def each(&block)
-      @resources.each(&block)
+      @routes.values.each(&block)
+    end
+
+    def last
+      @routes.values.last
     end
 
     def glob(pattern = "**/**")
@@ -21,25 +25,41 @@ module Mascot
 
     def get(request_path)
       return if request_path.nil?
-      find { |r| r.request_path == File.join("/", request_path) }
+      @routes[key(request_path)]
     end
 
     def add(resource)
-      @resources.push resource
+      return if resource.nil?
+      resource.add_observer self
+      @routes[key(resource)] = resource
+    end
+
+    def update(resource, old_request_path)
+      return if resource.nil?
+      @routes.delete key(old_request_path)
+      @routes[key(resource)] = resource
     end
 
     def remove(resource)
-      @resources.delete resource
+      return if resource.nil?
+      resource.delete_observer self
+      @routes.delete key(resource)
+      resource
     end
 
     def add_asset(asset, request_path: nil)
-      add Resource.new asset: asset, request_path: format_request_path(request_path || asset.path)
+      add Resource.new asset: asset, request_path: asset_path_to_request_path(request_path || asset.path)
     end
 
     private
+    def key(path)
+      raise "invalid key" if path.nil?
+      path = path.request_path if path.respond_to? :request_path
+      File.join("/", path)
+    end
     # Given a @file_path of `/hi`, this method changes `/hi/there/friend.html.erb`
     # to an absolute `/there/friend` format by removing the file extensions
-    def format_request_path(path)
+    def asset_path_to_request_path(path)
       # Relative path of resource to the file_path of this project.
       relative_path = Pathname.new(path).relative_path_from(@root_file_path)
       # Removes the .fooz.baz
