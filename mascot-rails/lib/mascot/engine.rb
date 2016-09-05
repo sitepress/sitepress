b5a4f053313668d103d1d8a33ed2d3588b6f6e4c
Brad Gessler <bradgessler@gmail.com>
2016-09-05 09:25:10 -0700

    Move Mascot::SiteController functionality into a module.

--- mascot-rails/lib/mascot/engine.rb
+++ mascot-rails/lib/mascot/engine.rb
@@ -3,5 +3,13 @@ module Mascot
     initializer "Add site root to view paths" do |app|
       ActionController::Base.prepend_view_path Mascot.configuration.site.root_path
     end
+
+    initializer "Require concerns path" do |app|
+      concerns_path = "app/controllers/concerns"
+
+      unless app.paths.keys.include?(concerns_path)
+        app.paths.add(concerns_path)
+      end
+    end
   end
 end
