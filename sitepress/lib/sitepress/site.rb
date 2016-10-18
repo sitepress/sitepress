9bcf4706f23d5a40c21a8d8ae4e3afab74bd221f
Brad Gessler <bradgessler@gmail.com>
2016-10-18 15:57:31 -0700

    Improve site documentation

--- sitepress/lib/sitepress/site.rb
+++ sitepress/lib/sitepress/site.rb
@@ -26,16 +26,13 @@ module Sitepress
       self.cache_resources = cache_resources
     end
 
-    # A tree representation of the resourecs wthin the site.
+    # A tree representation of the resourecs wthin the site. The root is a node that's
+    # processed by the `resources_pipeline`.
     def root
       ResourcesNode.new.tap do |node|
         DirectoryCollection.new(assets: pages_assets, path: pages_path).mount(node)
         resources_pipeline.process node
       end
-    end
-
-    def pages_path
-      root_path.join("pages")
     end
 
     # Returns a list of all the resources within #root.
@@ -44,16 +41,51 @@ module Sitepress
       @resources ||= ResourceCollection.new(node: root, root_path: root_path)
     end
 
+    # Root path to website project. Contains helpers, pages, and more.
     def root_path=(path)
       @root_path = Pathname.new(path)
     end
 
+    # Location of website pages.
+    def pages_path
+      root_path.join("pages")
+    end
+
     # Quick and dirty way to manipulate resources in the site without
-    # creating classes that implement the #process_resources method
+    # creating classes that implement the #process_resources method.
+    #
+    # A common example may be adding data to a resource if it begins with a
+    # certain path:
+    #
+    # ```ruby
+    # Sitepress.site.manipulate do |resource, root|
+    #   if resource.request_path.start_with? "/videos/"
+    #     resource.data["layout"] = "video"
+    #   end
+    # end
+    # ```
+    #
+    # A more complex, contrived example that sets index.html as the root node
+    # in the site:
+    #
+    # ```ruby
+    # Sitepress.site.manipulate do |resource, root|
+    #   if resource.request_path == "/index"
+    #     # Remove the HTML format of index from the current resource level
+    #     # so we can level it up.
+    #     node = resource.node
+    #     node.formats.remove ".html"
+    #     node.remove
+    #     root.add path: "/", asset: resource.asset # Now we can get to this from `/`.
+    #   end
+    # end
+    # ```
     def manipulate(&block)
       resources_pipeline << Extensions::ProcManipulator.new(block)
     end
 
+    # An array of procs that manipulate the tree and resources from the
+    # ResourceNode returned by #root.
     def resources_pipeline
       @resources_pipeline ||= ResourcesPipeline.new
     end
