df9552a9afc54cd7e577ad1c7919565f50a8d7df
Brad Gessler <bradgessler@gmail.com>
2016-11-09 14:36:06 -0800

    Parameterize stdout in compiler to reduce test verbosity

--- sitepress-cli/lib/sitepress/compiler.rb
+++ sitepress-cli/lib/sitepress/compiler.rb
@@ -6,26 +6,26 @@ module Sitepress
   class Compiler
     include FileUtils
 
-    def initialize(site: )
+    def initialize(site:, stdout: $stdout)
       @site = site
+      @stdout = stdout
     end
 
     # Iterates through all pages and writes them to disk
     def compile(target_path:)
       target_path = Pathname.new(target_path)
-      # TODO: Should file operations go here? Probably not.
       mkdir_p target_path
       root = Pathname.new("/")
-      puts "Compiling #{@site.root_path.expand_path}"
+      @stdout.puts "Compiling #{@site.root_path.expand_path}"
       @site.resources.each do |resource|
         # These are root `resource.request_path`
         derooted = Pathname.new(resource.request_path).relative_path_from(root)
         path = target_path.join(derooted)
         mkdir_p path.dirname
-        puts "  #{path}"
+        @stdout.puts "  #{path}"
         File.open(path.expand_path, "w"){ |f| f.write render(resource) }
       end
-      puts "Successful compilation to #{target_path.expand_path}"
+      @stdout.puts "Successful compilation to #{target_path.expand_path}"
     end
 
     private
