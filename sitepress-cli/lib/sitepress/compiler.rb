991f6605964db4e1cfaf93ddfa86c4240960b7e1
Brad Gessler <brad@polleverywhere.com>
2021-02-01 15:27:58 -0800

    Clean up Resource#request_path and Resource#compilation_path
    I still am not a huge fan of how these are bolted into the Resource
    object; it has a funkly smell that suggests it needs to be broken out
    into a seperate concern, but the function is more readable now until
    the day comes to refactor all of that.

--- sitepress-cli/lib/sitepress/compiler.rb
+++ sitepress-cli/lib/sitepress/compiler.rb
@@ -6,28 +6,21 @@ module Sitepress
   class Compiler
     include FileUtils
 
-    def initialize(site:, stdout: $stdout, default_page_name: "index.html")
+    def initialize(site:, stdout: $stdout)
       @site = site
       @stdout = stdout
-      @default_page_name = default_page_name
     end
 
     # Iterates through all pages and writes them to disk
     def compile(target_path:)
       target_path = Pathname.new(target_path)
       mkdir_p target_path
-      root = Pathname.new("/")
       cache_resources = @site.cache_resources
       @stdout.puts "Compiling #{@site.root_path.expand_path}"
       begin
         @site.cache_resources = true
         @site.resources.each do |resource|
-          derooted = Pathname.new(resource.request_path).relative_path_from(root)
-          path = if resource.format.nil?
-            target_path.join(derooted, @default_page_name)
-          else
-            target_path.join(derooted)
-          end
+          path = target_path.join(resource.compilation_path)
           mkdir_p path.dirname
           @stdout.puts "  #{path}"
           File.open(path.expand_path, "w"){ |f| f.write render(resource) }
