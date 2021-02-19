1bd4eacbed4864206c81c0809ec502f3637bef68
Brad Gessler <brad@polleverywhere.com>
2021-02-19 10:11:16 -0800

    Move sitepress config file for default Sitepress site

--- sitepress-cli/templates/default/site.rb
+++
@@ -1,6 +0,0 @@
-# Default layout for Sitepress pages
-Sitepress.configure do |config|
-  ## Change the root_path of the Sitepress site, or set to a different
-  ## Sitepress instance.
-  # config.site = Sitepress::Site.new root_path: "."
-end
