5cc042e69b59aa951f81182584736151a900fd26
Brad Gessler <brad@polleverywhere.com>
2021-03-03 17:48:19 -0800

    Add better error messaging and documentation

--- sitepress-rails/lib/sitepress/renderers/server.rb
+++ sitepress-rails/lib/sitepress/renderers/server.rb
@@ -1,5 +1,10 @@
 module Sitepress
   module Renderers
+    # Renders pages by invoking a rack call to the Rails application. From my
+    # experiments rendering as of 2021, this is the most reliable way to render
+    # pages. Rendering via `Renderers::Controller` has lots of various subtle issues
+    # that are surprising. People don't like surprises, so I opted to render through a
+    # slightly heavier stack.
     class Server
       attr_reader :rails_app, :page
 
@@ -11,8 +16,11 @@ module Sitepress
       def render
         code, headers, response = rails_app.routes.call env
         response.body
+      rescue => e
+        raise Compiler::RenderingError.new "Error rendering #{page.request_path.inspect} at #{page.asset.path.expand_path.to_s.inspect}:\n#{e.message}"
       end
 
+      private
       def env
         {
           "PATH_INFO"=> page.request_path,
