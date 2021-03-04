773faca788f6a9af71c366a1db2df69dd08dcb18
Brad Gessler <brad@polleverywhere.com>
2021-03-03 17:23:37 -0800

    Sort paths in core RSpec matchers for better cross-platform pass rates

--- sitepress-core/spec/spec_helper.rb
+++ sitepress-core/spec/spec_helper.rb
@@ -8,7 +8,7 @@ SimpleCov.start
 # TODO: Move into a support file.
 RSpec::Matchers.define :have_children do |expected|
   match do |actual|
-    actual.children.map(&:formats).map(&:to_a).flatten.map(&:request_path) == expected
+    actual.children.map(&:formats).map(&:to_a).flatten.map(&:request_path).sort == expected.sort
   end
   failure_message do |actual|
     "expected children #{actual.children.map(&:formats).map(&:to_a).flatten.map(&:request_path)} to be #{expected}"
@@ -17,7 +17,7 @@ end
 
 RSpec::Matchers.define :have_siblings do |expected|
   match do |actual|
-    actual.siblings.map(&:formats).map(&:to_a).flatten.map(&:request_path) == expected
+    actual.siblings.map(&:formats).map(&:to_a).flatten.map(&:request_path).sort == expected.sort
   end
   failure_message do |actual|
     "expected siblings #{actual.siblings.map(&:formats).map(&:to_a).flatten.map(&:request_path)} to be #{expected}"
@@ -26,7 +26,7 @@ end
 
 RSpec::Matchers.define :have_parents do |expected|
   match do |actual|
-    actual.parents.map(&:formats).map(&:to_a).flatten.map{ |r| r.request_path} == expected
+    actual.parents.map(&:formats).map(&:to_a).flatten.map{ |r| r.request_path}.sort == expected.sort
   end
   failure_message do |actual|
     "expected parent #{actual.parents.map(&:formats).map(&:to_a).flatten.map(&:request_path)} to be #{expected}"
