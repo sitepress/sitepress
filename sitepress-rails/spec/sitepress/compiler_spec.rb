567a98814d3074f4bf70f99c6eb2011ca3b4f862
Brad Gessler <bradgessler@gmail.com>
2021-03-15 23:59:15 -0700

    Fix failing compiler spec

--- sitepress-rails/spec/sitepress/compiler_spec.rb
+++ sitepress-rails/spec/sitepress/compiler_spec.rb
@@ -4,16 +4,16 @@ require "fileutils"
 
 describe Sitepress::Compiler do
   let(:site) { Sitepress.site }
-  let(:target) { Pathname.new(Dir::tmpdir).join("build") }
+  let(:build_path) { Pathname.new(Dir::tmpdir).join("build") }
   # Write compiler output to /dev/null so our test output remains clean.
   let(:stdout) { File.open(File::NULL, "w")  }
-  subject { Sitepress::Compiler.new(site: site, stdout: stdout) }
+  subject { Sitepress::Compiler.new(site: site, stdout: stdout, root_path: build_path) }
   describe "#compile" do
-    before { FileUtils.mkdir_p(target) }
-    after { FileUtils.rm_rf(target) }
-    it "writes files to target" do
-      subject.compile(target_path: target)
-      expect(Dir.glob(target.join("**")).size).to eql(2) # 2 items in the site... mkay?
+    before { FileUtils.mkdir_p(build_path) }
+    after { FileUtils.rm_rf(build_path) }
+    it "writes files to build_path" do
+      subject.compile
+      expect(Dir.glob(build_path.join("**")).size).to eql(2) # 2 items in the site... mkay?
     end
   end
 end
