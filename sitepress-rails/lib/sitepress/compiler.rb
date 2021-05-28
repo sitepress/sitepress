c5b737ff327d1e44dd26be8298897040877ecdb1
Brad Gessler <brad@polleverywhere.com>
2021-05-28 15:10:53 -0700

    Add `raise_exception_on_error` option to Compiler
    While I don't recommend turning this off, it is useful if you want to see
    how many resources are not compiling properly (instead of going through
    them one by one)

--- sitepress-rails/lib/sitepress/compiler.rb
+++ sitepress-rails/lib/sitepress/compiler.rb
@@ -8,10 +8,17 @@ module Sitepress
 
     attr_reader :site, :root_path
 
-    def initialize(site:, root_path:, stdout: $stdout)
+    # If a resource can't render, it will raise an exception and stop the compiler. Sometimes
+    # its useful to turn off errors so you can get through a full compilation and see how many
+    # errors you encounter along the way. To do that, you'd set `raise_exception_on_error` to
+    # `false` and the compile will get through all the resources.
+    attr_accessor :raise_exception_on_error
+
+    def initialize(site:, root_path:, stdout: $stdout, raise_exception_on_error: true)
       @site = site
       @stdout = stdout
       @root_path = Pathname.new(root_path)
+      @raise_exception_on_error = raise_exception_on_error
     end
 
     # Iterates through all pages and writes them to disk
@@ -27,7 +34,7 @@ module Sitepress
         end
       rescue
         status "Error building #{resource.inspect}"
-        raise
+        raise if raise_exception_on_error
       end
       status "Successful build to #{root_path.expand_path}"
     end
