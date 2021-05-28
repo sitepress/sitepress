27d69bd139b1ff3d6a59b4e000613bf7c7359667
Brad Gessler <brad@polleverywhere.com>
2021-05-28 12:55:25 -0700

    Add pry to spec helpers to make debugging tests easier

--- sitepress-rails/spec/spec_helper.rb
+++ sitepress-rails/spec/spec_helper.rb
@@ -3,7 +3,8 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path("../dummy/config/environment.rb", __FILE__)
-require 'rspec/rails'
+require "rspec/rails"
+require "pry"
 
 Rails.backtrace_cleaner.remove_silencers!
 
--- sitepress-server/spec/spec_helper.rb
+++ sitepress-server/spec/spec_helper.rb
@@ -1,1 +1,3 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require "pry"
