6103584134660c63e7babf0b9c54cce3b19d1da8
Brad Gessler <brad@polleverywhere.com>
2021-06-08 12:23:36 -0700

    Allow configuration of the NodeMapper by users.

--- sitepress-core/lib/sitepress/site.rb
+++ sitepress-core/lib/sitepress/site.rb
@@ -11,8 +11,12 @@ module Sitepress
     # Default root_path for site.
     DEFAULT_ROOT_PATH = Pathname.new(".").freeze
 
+    # Maps Rail-ish template files & structures into the site's node tree.
+    DEFAULT_NODE_MAPPER = AssetNodeMapper
+
     attr_reader :root_path
     attr_writer :resources_pipeline
+    attr_accessor :node_mapper
 
     # TODO: Get rid of these so that folks have ot call site.resources.get ...
     extend Forwardable
@@ -20,13 +24,14 @@ module Sitepress
 
     def initialize(root_path: DEFAULT_ROOT_PATH)
       self.root_path = root_path
+      self.node_mapper = DEFAULT_NODE_MAPPER
     end
 
     # A tree representation of the resourecs wthin the site. The root is a node that's
     # processed by the `resources_pipeline`.
     def root
       @root ||= Node.new.tap do |node|
-        AssetNodeMapper.new(path: pages_path, node: node).map
+        node_mapper.new(path: pages_path, node: node).map
         resources_pipeline.process node
       end
     end
