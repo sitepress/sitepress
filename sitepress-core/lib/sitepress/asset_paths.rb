f25d1bff12914a86df97c3c5e6e39d40ff52258a
Brad Gessler <brad@polleverywhere.com>
2021-06-08 12:22:36 -0700

    Fix bug in IGNORE_FILES_PATTERN
    I was trying to match on the whole path, so instead I call for the basename
    and then ignore.

--- sitepress-core/lib/sitepress/asset_paths.rb
+++ sitepress-core/lib/sitepress/asset_paths.rb
@@ -15,7 +15,8 @@ module Sitepress
       "*.orig"      # Created when there's a git conflict
     ]
 
-    # Partial rails prefix.
+    # Template files that start with `_user.html.erb` are partials that we want
+    # to ignore for the site's navigation tree.
     PARTIAL_PREFIX = "_".freeze
 
     attr_reader :path
@@ -27,8 +28,7 @@ module Sitepress
     # Returns a list of files, paths, and node names to iterate through to build out nodes
     def each
       path.each_child do |path|
-        next if ignore_file? path
-        yield path
+        yield path unless ignore_file? path
       end
     end
 
@@ -43,7 +43,7 @@ module Sitepress
     end
 
     def matches_ignore_file_pattern?(path)
-      IGNORE_FILE_PATTERNS.any? { |pattern| path.fnmatch? pattern }
+      IGNORE_FILE_PATTERNS.any? { |pattern| path.basename.fnmatch? pattern }
     end
   end
 end
