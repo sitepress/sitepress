3278a8116bd4e8810e274b7a4b075269abfdcaed
Brad Gessler <bradgessler@gmail.com>
2022-02-08 10:03:59 -0800

    Remove errant `config.routes=`

--- sitepress-cli/lib/sitepress/boot.rb
+++ sitepress-cli/lib/sitepress/boot.rb
@@ -6,7 +6,5 @@ require "sitepress/server"  # Load all the stuff needed setup the configuration 
 # Setup defaults for stand-alone Sitepress server in the current path. This
 # can, and should, be over-ridden by the end-user in the `config/site.rb` file.
 Sitepress.configure do |config|
-  config.routes = false
   config.site = Sitepress::Site.new root_path: "."
 end
-
