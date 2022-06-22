8c980019f7ccb357e68e35722f0465a26a3d93da
Brad Gessler <bradgessler@gmail.com>
2022-06-22 09:54:55 -0700

    Fix render_layout helper

--- sitepress-rails/rails/lib/generators/sitepress/install/templates/helpers/page_helper.rb
+++ sitepress-rails/rails/lib/generators/sitepress/install/templates/helpers/page_helper.rb
@@ -24,6 +24,6 @@ module PageHelper
   # Render a block within a layout. This is a useful, and prefered way, to handle
   # nesting layouts, within Sitepress.
   def render_layout(layout, **kwargs, &block)
-    render inline: capture(&block), layout: "layouts/#{layout}", **kwargs
+    render html: capture(&block), layout: "layouts/#{layout}", **kwargs
   end
 end
