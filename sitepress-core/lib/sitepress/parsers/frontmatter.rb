c3d17262ce344f0681c0c9fdb3c6797e37750e64
Brad Gessler <bradgessler@gmail.com>
2022-12-05 21:49:03 -0800

    Add permitted classes and YAML.safe_load to Frontmatter

--- sitepress-core/lib/sitepress/parsers/frontmatter.rb
+++ sitepress-core/lib/sitepress/parsers/frontmatter.rb
@@ -1,12 +1,17 @@
 require "yaml"
+require "date"
 
 module Sitepress
   module Parsers
     # Parses metadata from the header of the page.
-
-    # TODO: Redo this to use File readline and pos to
-    # perform faster
     class Frontmatter < Base
+      # Default classes that we'll allow YAML to parse. Ideally this doesn't
+      # get too huge and we let users control it by setting
+      # `Sitepress::Parsers::Frontmatter.permitted_classes = [Whatever, SuperDanger]`
+      PERMITTED_CLASSES = [
+        Date # Lots of blogs parse date front matter, so let this fly.
+      ]
+
       DELIMITER = "---".freeze
       NEWLINE = /\r\n?|\n/.freeze
       PATTERN = /\A(#{DELIMITER}#{NEWLINE}(.+?)#{NEWLINE}#{DELIMITER}#{NEWLINE}*)?(.+)\Z/m
@@ -16,7 +21,24 @@ module Sitepress
       end
 
       def data
-        @data ? YAML.load(@data) : {}
+        @data ? load_yaml(@data) : {}
+      end
+
+      def load_yaml(data)
+        if YAML.respond_to? :safe_load
+          YAML.safe_load data, permitted_classes: self.class.permitted_classes
+        else
+          # Live dangerously, lol
+          YAML.load data
+        end
+      end
+
+      class << self
+        attr_writer :permitted_classes
+
+        def permitted_classes
+          @permitted_classes ||= PERMITTED_CLASSES
+        end
       end
     end
   end
