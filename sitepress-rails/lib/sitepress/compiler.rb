6308906e0bb5cb0e26ac42a65db92e2d84a1545a
Brad Gessler <bradgessler@gmail.com>
2022-12-13 14:04:39 -0800

    Update compiler to have better output and build flags

--- sitepress-rails/lib/sitepress/compiler.rb
+++ sitepress-rails/lib/sitepress/compiler.rb
@@ -6,19 +6,21 @@ module Sitepress
   class Compiler
     include FileUtils
 
-    attr_reader :site, :root_path
+    attr_reader :site, :root_path, :failed, :succeeded
 
     # If a resource can't render, it will raise an exception and stop the compiler. Sometimes
     # its useful to turn off errors so you can get through a full compilation and see how many
-    # errors you encounter along the way. To do that, you'd set `raise_exception_on_error` to
+    # errors you encounter along the way. To do that, you'd set `fail_on_error` to
     # `false` and the compile will get through all the resources.
-    attr_accessor :raise_exception_on_error
+    attr_accessor :fail_on_error
 
-    def initialize(site:, root_path:, stdout: $stdout, raise_exception_on_error: true)
+    def initialize(site:, root_path:, stdout: $stdout, fail_on_error: true)
       @site = site
       @stdout = stdout
       @root_path = Pathname.new(root_path)
-      @raise_exception_on_error = raise_exception_on_error
+      @fail_on_error = fail_on_error
+      @failed = []
+      @succeeded = []
     end
 
     # Iterates through all pages and writes them to disk
@@ -32,11 +34,13 @@ module Sitepress
           status "Copying #{path}"
           cp resource.asset.path, path.expand_path
         end
+        @succeeded << resource
       rescue
         status "Error building #{resource.inspect}"
-        raise if raise_exception_on_error
+        @failed << resource
+        raise if fail_on_error
       end
-      status "Successful build to #{root_path.expand_path}"
+      status "Build at #{root_path.expand_path}"
     end
 
     private
