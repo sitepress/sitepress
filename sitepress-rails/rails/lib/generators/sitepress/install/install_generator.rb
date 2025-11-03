37de2011f0c184b2150211b8eabef71f271938c2
Brad Gessler <bradgessler@gmail.com>
2025-11-03 14:57:10 -0800

    Install markdown-rails gem in sitepress rails by default

--- sitepress-rails/rails/lib/generators/sitepress/install/install_generator.rb
+++ sitepress-rails/rails/lib/generators/sitepress/install/install_generator.rb
@@ -2,6 +2,8 @@ module Sitepress
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path("templates", __dir__)
 
+    class_option :skip_markdown, type: :boolean, default: false, desc: "Skip adding markdown-rails gem"
+
     def copy_files
       directory ".", "app/content"
     end
@@ -15,6 +17,14 @@ module Sitepress
       if File.exist? 'config/tailwind.config.js'
         inject_into_file 'config/tailwind.config.js', ",\n    './app/content/**/*.{erb,haml,html,slim,rb}'",
           after: "    './app/views/**/*.{erb,haml,html,slim}'"
+      end
+    end
+
+    def add_markdown_rails_gem
+      unless options[:skip_markdown]
+        gem "markdown-rails"
+        say "Added markdown-rails gem to Gemfile", :green
+        say "Run 'bundle install' to install the gem", :yellow
       end
     end
   end
